a617ef6d43b68e02333aacadb2dd073eca95535b
pondduck-beep <pond000duck@gmail.com>
2026-03-15 12:09:02 +0700

    Update IG.lua

--- IG.lua
+++ IG.lua
@@ -110,57 +110,57 @@ end
 while true do
 
 	portal("Starter")
-	task.wait(0.6)
+	task.wait(0.3)
 	fasttp(CFrame.new(174.26943969726562,11.206908226013184,-156.84486389160156))
-	task.wait(1.5)
+	task.wait(2)
 
 	portal("Jungle")
-	task.wait(0.6)
+	task.wait(0.3)
 	fasttp(CFrame.new(-563.8446655273438,-0.874668300151825,400.6252197265625))
-	task.wait(1.5)
+	task.wait(2)
 
 	portal("Desert")
-	task.wait(0.6)
+	task.wait(0.3)
 	fasttp(CFrame.new(-791.0394287109375,-4.222726821899414,-432.0268859863281))
-	task.wait(1.5)
+	task.wait(2)
 
 	portal("Snow")
-	task.wait(0.6)
+	task.wait(0.3)
 	fasttp(CFrame.new(-407.5099792480469,-1.1388468742370605,-990.491455078125))
-	task.wait(1.5)
+	task.wait(2)
 
 	portal("Shibuya")
-	task.wait(0.6)
+	task.wait(0.3)
 	fasttp(CFrame.new(1400.0594482421875,8.486136436462402,484.9847106933594))
-	task.wait(1.5)
+	task.wait(2)
 
 	portal("HuecoMundo")
-	task.wait(0.6)
+	task.wait(0.3)
 	fasttp(CFrame.new(-369.4567565917969,-0.15934494137763977,1092.5155029296875))
-	task.wait(1.5)
+	task.wait(2)
 
 	portal("Shinjuku")
-	task.wait(0.6)
+	task.wait(0.3)
 	fasttp(CFrame.new(-17.37151527404785,1.8984041213989258,-1842.671630859375))
-	task.wait(1.5)
+	task.wait(2)
 
 	fasttp(CFrame.new(666.2935180664062,1.8831005096435547,-1692.1214599609375))
-	task.wait(1.5)
+	task.wait(2)
 
 	portal("Slime")
-	task.wait(0.6)
+	task.wait(0.3)
 	fasttp(CFrame.new(-1123.855224609375,13.91822624206543,368.31768798828125))
-	task.wait(1.5)
+	task.wait(2)
 
 	portal("Academy")
-	task.wait(0.6)
+	task.wait(0.3)
 	fasttp(CFrame.new(1068.37646484375,1.778355360031128,1277.8568115234375))
-	task.wait(1.5)
+	task.wait(2)
 
 	portal("Judgement")
-	task.wait(0.6)
+	task.wait(0.3)
 	fasttp(CFrame.new(-1270.6287841796875,1.177457332611084,-1192.44189453125))
-	task.wait(1.5)
+	task.wait(2)
 
 
 end
